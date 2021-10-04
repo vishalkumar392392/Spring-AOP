66f61dcd3c17d2bf94f251b9c36765036b25fd55
vishal palla <vishalpalla27@gmail.com>
2021-10-04 22:11:55 +0530

    added first commit

--- src/main/java/com/vishal/spring/aop/aspect/AroundAspect.java
+++ src/main/java/com/vishal/spring/aop/aspect/AroundAspect.java
@@ -3,6 +3,7 @@ package com.vishal.spring.aop.aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -24,16 +25,21 @@ public class AroundAspect {
 		logger.info("Time Taken by {} is {}",joinPoint,timeTaken);
 	}
 	
-//	@Around("com.vishal.spring.aop.aspect.CommonJoinPointConfig.trackTimeAnnotation()")
-//	public void aroundwithAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
-//        
-//		long startTime = System.currentTimeMillis();
-//		
-//		joinPoint.proceed();
-//		
-//		long timeTaken = System.currentTimeMillis() - startTime;
-//		logger.info("Time Taken by {} is {}",joinPoint,timeTaken);
-//	}
+	@Pointcut("@within(track)")
+	public void underTraceEnterExit(Track track) {
+		// declaration
+	}
+	
+	@Around("underTraceEnterExit(track)")
+	public void aroundwithAnnotation(ProceedingJoinPoint joinPoint, Track track) throws Throwable {
+        
+		long startTime = System.currentTimeMillis();
+		
+		joinPoint.proceed();
+		
+		long timeTaken = System.currentTimeMillis() - startTime;
+		logger.info("Time Taken by {} is {}",joinPoint,timeTaken);
+	}
 	
 	//com.vishal.spring.aop.aspect.CommonJoinPointConfig.trackTimeAnnotation()
 
--- src/main/java/com/vishal/spring/aop/bussiness/Bussiness1.java
+++ src/main/java/com/vishal/spring/aop/bussiness/Bussiness1.java
@@ -3,6 +3,7 @@ package com.vishal.spring.aop.bussiness;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.vishal.spring.aop.aspect.Track;
 import com.vishal.spring.aop.data.Dao;
 
 @Service
@@ -11,6 +12,7 @@ public class Bussiness1 {
 	@Autowired
 	private Dao dao;
 	
+	@Track
 	public String calculateSomething() {
 		return dao.retrieveSomething();
 	}
